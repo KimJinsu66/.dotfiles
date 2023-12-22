0cfd7b0bb381b168b53568330cb6416905feda7e
KimJinsu66 <j_kim@ga-tech.co.jp>
2023-12-22 23:43:23 +0900

    oh my zshを追加

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,7 +1,19 @@
 ln -s ~/.dotfiles/zshrc/.zshrc ~
 
+# oh my zshを先にdownloadする必要がある
+# https://ohmyz.sh/#install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # 開発で必要なライブラリInstall
 brew install gh
+brew install ripgrep # installしないとvim設定でエラー発生、検索で利用している
+
 
 # powerlevel10k terminalデザインのため修正
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH/themes/powerlevel10k
+
+open "https://kangyb.tistory.com/23" # ちゃんとまとまっている(韓国語)
+
+# zsh-autosuggestions install
+# リンク: https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md#oh-my-zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
