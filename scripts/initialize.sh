a23bcd24a3d9ca2e15634f8aba00ca1797809920
KimJinsu66 <j_kim@ga-tech.co.jp>
2023-12-22 22:33:44 +0900

    scriptに gh commandと10k pack installコマンド追加

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,1 +1,7 @@
 ln -s ~/.dotfiles/zshrc/.zshrc ~
+
+# 開発で必要なライブラリInstall
+brew install gh
+
+# powerlevel10k terminalデザインのため修正
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH/themes/powerlevel10k
