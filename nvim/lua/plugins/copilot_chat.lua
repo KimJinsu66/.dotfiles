6cf885a0c5a170c3b850502931a57b8542038612
KimJinsu66 <jinsu6688@gmail.com>
2025-04-19 10:15:19 +0900

    copilot chatのdefaultモデルをgeminiに設定

--- nvim/lua/plugins/copilot_chat.lua
+++ nvim/lua/plugins/copilot_chat.lua
@@ -7,6 +7,7 @@ return {
     },
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
+      model= "gemini-2.0-flash-001"
       -- See Configuration section for options
     },
     -- See Commands section for default commands if you want to lazy load on them
